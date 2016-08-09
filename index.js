2dc3bcbfc9cdddc6108ef891987e638e6864339b
hudson-prestidge <charcon@ihug.co.nz>
2016-08-10 10:37:43 +1200

    added babelify, some es6 features

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var xhr = require('xhr')
-var greeting = require('./views/greeting.hbs')
-var visibility = require('./views/visibility.hbs')
+const xhr = require('xhr')
+const greeting = require('./views/greeting.hbs')
+const visibility = require('./views/visibility.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
+const endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
 
-  var target = document.getElementsByTagName('main')[0]
+  const target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting(JSON.parse(data.body))
   document.getElementById('awesome-button')
     .addEventListener('click', getVisibility)
@@ -21,7 +21,7 @@ function getVisibility(){
       console.error(err)
     }
 
-    var target = document.getElementsByTagName('main')[0]
+    const target = document.getElementsByTagName('main')[0]
     target.innerHTML += visibility(JSON.parse(data.body))
   })
 }
