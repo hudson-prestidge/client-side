f71bb7f26108d66df02761f76605ca066910e8af
hudson-prestidge <charcon@ihug.co.nz>
2016-08-09 16:03:03 +1200

    really big commit

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var visibility = require('./views/visibility.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-
-  // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+  target.innerHTML = greeting(JSON.parse(data.body))
+  document.getElementById('awesome-button')
+    .addEventListener('click', getVisibility)
 })
+
+function getVisibility(){
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
+
+    var target = document.getElementsByTagName('main')[0]
+    target.innerHTML += visibility(JSON.parse(data.body))
+  })
+}
--- tests/greeting.js
+++ tests/greeting.js
@@ -4,18 +4,31 @@ var fs = require('fs')
 var path = require('path')
 var cheerio = require('cheerio')
 
+// test('greeting view', function(t) {
+//   var templateText = fs.readFileSync(path.join(__dirname, 'views/greeting.hbs'), 'utf8')
+//   var templateFunction = Handlebars.compile( templateText )
+//
+//   var exampleData = {name: 'MIXMIX'}
+//
+//   var renderedTemplate = templateFunction(exampleData)
+//   var $ = cheerio.load(renderedTemplate)
+//
+//   t.equal($('h1').text(), "Hello MIXMIX!", 'template uses data in heading')
+//
+//   t.end()
+// })
+
 test('greeting view', function(t) {
-  var templateText = fs.readFileSync(path.join(__dirname, 'views/greeting.hbs'), 'utf8')
+  var templateText = fs.readFileSync(path.join(__dirname, '../views/greeting.hbs'), 'utf8')
   var templateFunction = Handlebars.compile( templateText )
 
-  var exampleData = {name: 'MIXMIX'}
+  var exampleData = {latitude: 12, longitude: 3}
 
   var renderedTemplate = templateFunction(exampleData)
   var $ = cheerio.load(renderedTemplate)
 
-  t.equal($('h1').text(), "Hello MIXMIX!", 'template uses data in heading')
+  t.equal($('#latitude').text(), "\n  Latitude: 12\n", 'template uses data in latitude')
+  t.equal($('#longitude').text(), "\n  Longitude: 3\n", 'template uses data in longitude')
 
   t.end()
 })
-
-
